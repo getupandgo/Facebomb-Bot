ab011a49e82270b2d1179444803597880a747668
getupandgo <getupandgo11@gmail.com>
2016-08-29 01:08:38 +0300

    more responses; parsing url and passing it to FaceBomberCore

--- FaceBomberModule/wrapper.h
+++ FaceBomberModule/wrapper.h
@@ -8,17 +8,17 @@
 
 class FaceBomberWrapper : public node::ObjectWrap {
  public:
-  static void Init(v8::Local<v8::Object> exports);
-  static void SayHello(const v8::FunctionCallbackInfo<v8::Value>& args);
+ static void Init(v8::Local<v8::Object> exports);
+ static void SayHello(const v8::FunctionCallbackInfo<v8::Value>& args);
 
  private:
  FaceBomber* bomber;
 
-  explicit FaceBomberWrapper();
-  ~FaceBomberWrapper();
+ explicit FaceBomberWrapper();
+ ~FaceBomberWrapper();
 
-  static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
-  static v8::Persistent<v8::Function> constructor;
+ static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
+ static v8::Persistent<v8::Function> constructor;
 };
 
 #endif
--- app.js
+++ app.js
@@ -1,27 +1,70 @@
+'use strict'
+
 const Botkit = require('botkit')
+
 const FaceBomber = require('./build/Release/FaceBomber')
-
-const obj = new FaceBomber.FaceBomberWrapper()
-obj.sayHello()
-
 const config = require('./config.json')
 
 const controller = Botkit.slackbot({
   debug: config.debug
 })
 
-const bot = controller.spawn({
+// spawning bot
+controller.spawn({
   token: config.bot_token
 }).startRTM()
 
+const bomber = new FaceBomber.FaceBomberWrapper()
+
 controller.hears(['hello', 'hi'], 'direct_mention,direct_message,mention', function (bot, message) {
+  bomber.sayHello() // TODO: debug one
   bot.reply(message, ':new_moon_with_face: :bomb:')
 })
 
 controller.hears(['facebomb'], ['direct_mention,direct_message'], function (bot, message) {
-  bot.reply(message, 'I\'m gonna do facebomb! :bomb:')
+  let pictureUrl = message.text.match(/(https?:\/\/.*\.(?:png|jpg))/i) // TODO: why 2 matches?
+  let response = ''
+
+  if (pictureUrl && pictureUrl.length) {
+    if (pictureUrl.length > 1) {
+      console.log('Applying only one picture at a time - getting first one')
+      pictureUrl = pictureUrl[0]
+    }
+
+    console.log('processing image...')
+    // const success = bomber.doFacebomb(pictureUrl)
+    // if (!success) {
+    //   console.log('no faces found!')
+    // }
+
+    response = 'I\'m gonna do facebomb! :bomb:'
+  } else {
+    response = 'Provide me url on image to bomb!'
+  }
+
+  bot.reply(message, response)
 })
 
 controller.hears(['add face to bomb'], ['direct_mention,direct_message'], function (bot, message) {
-  bot.reply(message, 'New victim! Wohooo! :unicorn_face:')
+  let faceUrl = message.text.match(/(https?:\/\/.*\.(?:png|jpg))/i) // TODO: why 2 matches?
+  let response = ''
+
+  if (faceUrl && faceUrl.length) {
+    if (faceUrl.length > 1) {
+      console.log('Applying only one face at a time - getting first one')
+      faceUrl = faceUrl[0]
+    }
+
+    console.log('processing image...')
+    // const success = bomber.addFaceToBomb(faceUrl)
+    // if (!success) {
+    //   console.log('no faces found!')
+    // }
+
+    response = 'New victim! Wohooo! :unicorn_face:'
+  } else {
+    response = 'Provide me url on my new victim!'
+  }
+
+  bot.reply(message, response)
 })
